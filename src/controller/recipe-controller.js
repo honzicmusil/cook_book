dbe9fce8100c1164d40032091f0bf7dafb54436f
Jan Musil <jan.musil@external.t-mobile.cz>
2021-05-31 09:39:56 +0100

    raw structure + routing

---
+++ src/controller/recipe-controller.js
@@ -0,0 +1,46 @@
+import * as Recipe from "../model/recipe"
+
+const list = async (req, res) => {
+    try {
+        const newRecipe = req.body;
+        res.status(200).json({});
+    }
+    catch (e) {
+        res.status(200).json({exception: e});
+    }
+}
+
+const create = async (req, res) => {
+    try {
+        const newRecipe = req.body;
+        res.status(200).json({});
+    }
+    catch (e) {
+        res.status(200).json({exception: e});
+    }
+}
+
+const update = async (req, res) => {
+    try {
+        const newRecipe = req.body;
+        res.status(200).json({});
+    }
+    catch (e) {
+        res.status(200).json({exception: e});
+    }
+}
+
+const remove = async (req, res) => {
+    try {
+        const newRecipe = req.body;
+        res.status(200).json({});
+    }
+    catch (e) {
+        res.status(200).json({exception: e});
+    }
+}
+
+exports.list = list
+exports.create = create
+exports.update = update
+exports.remove = remove
