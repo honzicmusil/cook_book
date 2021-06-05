b3b5e1c2a9e8d941f6c15e1c34953b5dfd37c4ed
jenik <JenikNo.4@gmail.com>
2021-06-05 10:02:11 +0200

    removed milotas legacy code..

--- nodejs/app.js
+++ nodejs/app.js
@@ -22,13 +22,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/recipe/", recipeController);
 app.use("/material/", materialController);
 
-app.use("/library-spa.js", function (req, res) {
-  res.sendFile(path.join(__dirname + "/hi/library-spa.js"));
-});
-// app.use("/book.js", function(req,res) {
-//     res.sendFile(path.join(__dirname+'/hi/book.js'))recipe
-// })
-
 app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname + "/index.html"));
 });
